4915607717ba8b1254c798ccf2f48bf0bc8db7e4
sa9i9f <seyfff39@gmail.com>
2021-09-29 00:10:30 +0300

    completed the task

--- task.js
+++ task.js
@@ -1,44 +1,31 @@
-let numbers = [
-    5,
-    8,
-    0,
-    1,
-    9,
-    11,
-    15,
-    16
-];
+const { readFileSync } = require("fs");
+fs = require("fs");
+let numbers = [5, 8, 0, 1, 9, 11, 15, 16];
+let nNumbers = numbers;
+let numbersFromFile = readFileSync("data.txt", "utf8");
 
-console.log("Original numbers list: ", numbers)
-
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] > numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
+// method to sort array of Int. using bubble sort alg. in Asc and Desc
+const bubbleSort = (array, ascending = true) => {
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length - 1; j++) {
+      if (ascending ? array[j] > array[j + 1] : array[j] < array[j + 1]) {
+        // j not i
+        [array[j], array[j + 1]] = [array[j + 1], array[j]]; // Short method of the swaping
+      }
     }
-}
+  }
+  return array;
+};
 
-console.log("Numbers list After sorting: ", numbers)
+// print ascending sort , descending sort   and wrte sort number   to output file
+console.log("Original numbers list: ", numbers);
+console.log("numbers list After sorting: ", bubbleSort(nNumbers));
+console.log(
+  "numbers list After Desc sorting:",
+  bubbleSort(nNumbers, (ascending = false))
+);
 
-for(let i = 0 ; i < numbers.length ; i++)
-{
-    for(let j = 0 ; j < numbers.length - 1 ; j++)
-    {
-        if(numbers[i] < numbers[j + 1])
-        {
-            let temp = numbers[j];
-            numbers[j] = numbers[j + 1]
-            numbers[j + 1] = temp;
-        }
-    }
-}
-
-console.log("Numbers list After Desc sorting: ", numbers)
-
-
+fs.writeFileSync(
+  "output.txt",
+  bubbleSort(numbersFromFile.split(",")).join(",")
+);
